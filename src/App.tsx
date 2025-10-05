faa8ab49763befccc86ca644dcb42954f9b1e195
AwesomeCoder412412 <44704454+AwesomeCoder412412@users.noreply.github.com>
2025-10-05 17:27:31 -0500

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -446,18 +446,19 @@ function SearchPage() {
     });
     const encoded = encodeURIComponent(hitsArr[0].title);
     let info = await fetch(BASE_URL + encoded + ".txt");
-    console.log (info);
+    console.log(await fetch(BASE_URL + encoded + ".txt"))
+    console.log(BASE_URL + encoded + ".txt");
+    console.log(docsRef.current.get(hitsArr[0].id)?.content);
 
     let aiBabble = "The user asked: " + q + "\n I am now going to give you the contents of several academic papers that are relevant to this topic. Use ONLY KNOWLEDGE FROM THE FOLLOWING PAPERS to answer the user's question. Every piece of information you get from the papers MUST BE CITED with the title of cited paper in parentheses at the end of the relevant sentences. Thank you very much. BEGIN PAPERS: "
     
     let count = 0;
-    while (count < 5 && count < hitsArr.length) {
+    while (count < 3 && count < hitsArr.length) {
         console.log ("On paper " + count);
-        const encoded = encodeURIComponent(hitsArr[count].title);
-        let info = await fetch(BASE_URL + encoded + ".txt");
-        aiBabble = aiBabble + "PAPER TITLE: " + hitsArr[count].title + " PAPER CONTENT: " + info + "END PAPER CONTENT. ", 
+        aiBabble = aiBabble + "PAPER TITLE: " + hitsArr[count].title + " PAPER CONTENT: " + await AIU("", "Please summarize the key statistics and points in this paper for another AI to be able to quickly read and get as much infomration out of this as possible: " + docsRef.current.get(hitsArr[count].id)?.content) + "END PAPER CONTENT. ", 
         count++;
     }
+    console.log(aiBabble);
 
    
     const hitsMap2 = new Map<string, SearchResult>();
