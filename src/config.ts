f23cc8d44c6360737ca68abbc4adaa4792f17e6d
Uma Malani <ugmalani@gmail.com>
2025-10-05 18:36:41 -0500

    kahgfk

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@
 // export const BASE_URL = "https://example.com/articles/"
 
 export const MANIFEST_URL = "https://raw.githubusercontent.com/AwesomeCoder412412/stupid/refs/heads/main/manifest.json"; // <-- set this to your manifest.json URL
+export const OUTPUT_URL = "https://github.com/AwesomeCoder412412/stupid/blob/main/output.json";
 export const BASE_URL = "https://raw.githubusercontent.com/AwesomeCoder412412/stupid/refs/heads/main/articles_text2/"; // <-- set this to the base folder URL (ensure trailing slash)
 
-// -----------------
+// -----------------x
