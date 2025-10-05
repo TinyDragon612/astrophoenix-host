88afa8c4adcbcfdc1f910d13cce2961b66be73ea
AwesomeCoder412412 <44704454+AwesomeCoder412412@users.noreply.github.com>
2025-10-05 18:36:15 -0500

    fddf

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@
 
 export const MANIFEST_URL = "https://raw.githubusercontent.com/AwesomeCoder412412/stupid/refs/heads/main/manifest.json"; // <-- set this to your manifest.json URL
 export const BASE_URL = "https://raw.githubusercontent.com/AwesomeCoder412412/stupid/refs/heads/main/articles_text2/"; // <-- set this to the base folder URL (ensure trailing slash)
-
+export const BASE_URL_URLS = "https://raw.githubusercontent.com/AwesomeCoder412412/stupid/refs/heads/main/articles_text_withurls/";
 // -----------------
