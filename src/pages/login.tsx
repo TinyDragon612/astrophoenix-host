86c1a040d0f5d58b5a64a08b134a40eb4cdd8ae1
Evie Hu <eviehu612@gmail.com>
2025-10-05 14:15:28 -0500

    pushhhhhhh

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,26 +8,7 @@ signInWithEmailAndPassword,
 } from "firebase/auth";
 
 import { setPersistence, browserLocalPersistence } from "firebase/auth";
-
-const firebaseConfig = {
-apiKey: "AIzaSyBKX0eIL-ja9zHCsgi_UwlD8JlByY96UB8",
-  authDomain: "astrophoenix2-3b620.firebaseapp.com",
-  projectId: "astrophoenix2-3b620",
-  storageBucket: "astrophoenix2-3b620.firebasestorage.app",
-  messagingSenderId: "335448449685",
-  appId: "1:335448449685:web:fe31fc84f0313fa075319c",
-  measurementId: "G-C9M23TNWHD"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-setPersistence(auth, browserLocalPersistence)
-  .then(() => {
-    // Now auth state persists across refresh
-  })
-  .catch((error) => {
-    console.error("Failed to set persistence:", error);
-  });
+import { auth } from "../firebase";
 
 const LoginSignup: React.FC = () => {
 const [isLogin, setIsLogin] = useState(true);
